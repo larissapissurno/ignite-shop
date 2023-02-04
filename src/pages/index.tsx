2305973a0859542a48e3e2eff785a21383462c29
Larissa Pissurno <larissapissurno@gmail.com>
2023-02-04 20:45:08 -0300

    chore: config stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "@/styles"
+
+const Button = styled("button", {
+  backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <Button>hello world</Button>
   )
 }
