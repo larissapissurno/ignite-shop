f2255da07ea5822d23b41288012df3756610a057
Larissa Pissurno <larissapissurno@gmail.com>
2023-02-05 10:29:22 -0300

    feat: home UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import logo from '@/assets/logo.svg'
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logo.src} alt="" />
+        <Image src={logo} alt="" />
       </Header>
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-const Button = styled("button", {
-  backgroundColor: '$green300'
-})
+import tshirt1 from '@/assets/tshirts/1.png'
+import tshirt2 from '@/assets/tshirts/2.png'
+import tshirt3 from '@/assets/tshirts/3.png'
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Button>hello world</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
