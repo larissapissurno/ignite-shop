3ff7178b05cef8879bc1cd3f4f9aff0dfe418ba5
Larissa Pissurno <larissapissurno@gmail.com>
2023-02-05 16:06:40 -0300

    feat: product page UI

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,24 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 89,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum a quae commodi animi. Saepe, placeat odit aliquam inventore minus cupiditate animi libero eos fugit architecto aliquid excepturi, harum, delectus ducimus.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
