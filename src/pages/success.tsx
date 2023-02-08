b7503713d7706ab70754c747f4b0bc7d3d8a3a50
Larissa Pissurno <larissapissurno@gmail.com>
2023-02-08 10:04:01 -0300

    feat: success page UI

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,27 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
+  const userName = 'Larissa Pissurno';
+  const productName = 'Camiseta Beyond the Limits';
+
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+      
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>{userName}</strong>, {' '}
+        sua <strong>{productName}</strong> {' '}
+        já está a caminho da sua casa. 
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
